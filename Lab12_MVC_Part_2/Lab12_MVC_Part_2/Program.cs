66f51ca353f020358ab35411da55f24cd70fb2db
ecaoile <unfie555@gmail.com>
2018-06-20 11:27:04 -0700

    finished work with sql server localdb

--- Lab12_MVC_Part_2/Lab12_MVC_Part_2/Program.cs
+++ Lab12_MVC_Part_2/Lab12_MVC_Part_2/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Lab12_MVC_Part_2.Models;
+using Lab12_MVC_Part_2;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Lab12_MVC_Part_2
 {
@@ -14,7 +17,26 @@ namespace Lab12_MVC_Part_2
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<Lab12_MVC_Part_2Context>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
