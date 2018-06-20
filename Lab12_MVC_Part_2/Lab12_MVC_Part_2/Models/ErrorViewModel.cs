5639e3debca40a4dd7bf8b1660ede86f3b555508
ecaoile <unfie555@gmail.com>
2018-06-20 09:57:04 -0700

    created MVC base

---
+++ Lab12_MVC_Part_2/Lab12_MVC_Part_2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab12_MVC_Part_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
