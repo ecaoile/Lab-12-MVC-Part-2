b3b1db30f3b911c032e5f3d1fb5cfff2261f952c
ecaoile <unfie555@gmail.com>
2018-06-20 12:18:39 -0700

    finished add search

--- Lab12_MVC_Part_2/Lab12_MVC_Part_2/Models/Movie.cs
+++ Lab12_MVC_Part_2/Lab12_MVC_Part_2/Models/Movie.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,8 +11,13 @@ namespace Lab12_MVC_Part_2.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
